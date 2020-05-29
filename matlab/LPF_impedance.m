35e25e0f7f178ff33b6b3f86e6e711ee2637ac80
codeman5199 <519cmccall@gmail.com>
2020-05-29 15:13:31 -0700

    updated matlab script w/ bug in n3 calc fixed

--- matlab/LPF_impedance.m
+++ matlab/LPF_impedance.m
@@ -1,16 +1,17 @@
 
 % LPF stub impedance calculation
 
-fc = 2*10^9;
+fc = 2*10^9;            %cutoff freq
 wc = (2*pi)*(fc);
-R0 = 50;
-N=5;
+R0 = 50;                %normalized to 50 Ohms
+N=5;                    %5th order
 a = 1:N;
 b = 1:N;
 g = 1:N;
 ripple = 0.2;
 beta = log(coth(ripple/17.37));
 y = sinh(beta/(2*N));
+%calculate a, b, and g values
 for i = 1:N
    a(i) = sin(((2*i-1)*pi)/(2*N)); 
    b(i) = (y^2) + (sin((i*pi)/N))^2;
@@ -21,20 +22,23 @@ for i = 1:N
    end
 end
 
+%lumped elements
 C1= g(1)/(R0*wc);
 L1 = (g(2)*R0)/wc;
 C2= g(3)/(R0*wc);
 L2 = (g(4)*R0)/wc;
 C3= g(5)/(R0*wc);
 
+%kuroda identities
 n1 = 1 + (g(1));                    %1 + cap/tline
-n2 = 1 + (1)/(1/n1);                %1 + tline/ind
-n3 = 1 + (1/(g(1)*n1))/(g(2));      %1 + tline/ind
-Tc1 = 1/n2;
-Ttl1 = n2/n1;
-Tc2 = n1/(n3*g(1));
-Ttl2 = n3*g(2);
+n2 = 1 + n1;                        %1 + tline/ind
+n3 = 1 + g(1)/(n1*g(2));            %1 + tline/ind
+Tc1 = 1/n2;                         %first stub
+Ttl1 = n2/n1;                       %first tline
+Tc2 = n1/(n3*g(1));                 %second stub
+Ttl2 = n3*g(2);                     %second tline
 
+%final impedances
 Z1 = 50*Tc1;
 Z2 = 50*Ttl1;
 Z3 = 50*Tc2;
